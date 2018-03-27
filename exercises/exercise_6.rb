e96106c191c646ac9e890a3261a244e1f230d659
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-03-27 18:20:00 +0000

    Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Philip", last_name: "Louie", hourly_rate: 40)
+@store1.employees.create(first_name: "Jose", last_name: "Rizal", hourly_rate: 50)
+@store1.employees.create(first_name: "Andres", last_name: "Bonifacio", hourly_rate: 30)
+@store1.employees.create(first_name: "Emilio", last_name: "Aguinaldo", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Joseph", last_name: "Estrada", hourly_rate: 45)
+@store2.employees.create(first_name: "Ninoy", last_name: "Aquino", hourly_rate: 50)
+@store2.employees.create(first_name: "Manny", last_name: "Pacquiao", hourly_rate: 35)
+@store2.employees.create(first_name: "Fidel", last_name: "Ramos", hourly_rate: 30)
+@store2.employees.create(first_name: "Rodrigo", last_name: "Duterte", hourly_rate: 50)
